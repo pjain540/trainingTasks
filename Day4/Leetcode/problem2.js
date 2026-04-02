9e83d93c27379b1df099ae010149a27a5a628f81
pjain540 <p.jain@braininventory@gmail.com>
2026-04-02 18:45:31 +0530

    day4 leetcode

--- Day4/Leetcode/problem2.js
+++ Day4/Leetcode/problem2.js
@@ -1,2 +1,19 @@
 //https://leetcode.com/problems/group-anagrams/description/
 //words are anagram if there sorted order is same
+
+function isAnagram(s, t) {
+    if (s.length !== t.length) return false;
+
+    let map = {};
+
+    for (let char of s) {
+        map[char] = (map[char] || 0) + 1;
+    }
+
+    for (let char of t) {
+        if (!map[char]) return false;
+        map[char]--;
+    }
+
+    return true;
+}
